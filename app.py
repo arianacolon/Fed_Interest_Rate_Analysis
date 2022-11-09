705af89f3e5fbaca1e542e72cf6fdff5163068e2
Adrian Maple <AdrianDMaple@outlook.com>
2022-11-08 19:04:16 -0500

    Adding Dashboard info

---
+++ app.py
@@ -0,0 +1,28 @@
+import csv 
+import json 
+
+
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+      
+    #read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+       
+  
+    #convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+        
+
+        
+csvFilePath = r'unemployment_table.csv'
+jsonFilePath = r'unemployment_data.json'
+csv_to_json(csvFilePath, jsonFilePath)          
